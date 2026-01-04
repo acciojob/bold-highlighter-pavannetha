06fe608ae1dc51d3ef115b495372b541a1c8ec11
pavannetha <chilukuripavan4185@gmail.com>
2026-01-04 08:26:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function highlight() {
     //Write your code here
-
+let allStong = document.getElementByTagName("strong")
+	allStong.style.color = "green"
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+    //Write your code here
+let allWeak = document.getElementByTagName("strong")
+	allWeak.style.color = "black"
     
 }
